3ea2a63e0b8328c64dc6c7c4b7ddac7fee686fc9
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 08:52:42 -0300

    Create ex 5.go

---
+++ ex 5.go
@@ -0,0 +1,10 @@
+1: Ler lista de arquivos
+2: Ler tamanho da lista
+3: Ler arquivo desejado, exemplo: "Algoritmo1.txt"
+4: Para cada arquivo da lista, faça:
+5: Numa aba de pesquisa, utilize o nome do arquivo desejado como referência
+6: Se:
+7: O nome do arquivo for compatível com o desejado,
+   escreva o nome do arquivo em destaque e execute.
+8: Senão:
+9: Continue a procura com o arquivo subsequente da lista
