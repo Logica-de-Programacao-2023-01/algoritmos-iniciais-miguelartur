965a61cb35ab0fef7fc10851cdc4b772719be3ce
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 08:51:49 -0300

    Create ex 2.go

---
+++ ex 2.go
@@ -0,0 +1,8 @@
+1. Leia temperatura
+2. Leia unidade
+3. Se unidade igual a Celsius:
+4. Resultado = temperatura * 1.8 + 32
+5. Escreva O resultado é resultado
+6. Senão:
+7. Resultado = (temperatura - 32)/1.8
+8. Escreva O resultado é resultado
