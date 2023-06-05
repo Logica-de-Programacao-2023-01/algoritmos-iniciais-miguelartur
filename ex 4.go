9ba889e8dfffcaae3195ca0479dd1a0e12af835b
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 08:52:22 -0300

    Create ex 4.go

---
+++ ex 4.go
@@ -0,0 +1,5 @@
+1: Ler y , de modo que y seja um número inteiro positivo diferente de 0
+2: Inicializar a variável fatorial com 1 
+3: Para cada número inteiro de 1 até x, faça:
+4: Atribua à variável fatorial o valor do número e multiplique por fatorial
+5: Escreva o resultado  
